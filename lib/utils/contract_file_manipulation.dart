ccd2ed9dd5ea0c9f32b3966e272b11770ba5ad99
Sarah Camargo <sarahcamargo00@gmail.com>
2024-08-19 16:52:59 -0300

    fix contract template

--- lib/utils/contract_file_manipulation.dart
+++ lib/utils/contract_file_manipulation.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:archive/archive.dart';
+import 'package:extenso/extenso.dart';
+import 'package:intl/intl.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:xml/xml.dart';
 
@@ -37,7 +39,8 @@ class ContractFileManipulation {
 
     final newContent = _replacePlaceholders(content, data);
 
-    final newContentFile = ArchiveFile('content.xml', utf8.encode(newContent).length, utf8.encode(newContent));
+    final newContentFile = ArchiveFile(
+        'content.xml', utf8.encode(newContent).length, utf8.encode(newContent));
 
     final newArchive = Archive();
     for (var file in archive.files) {
@@ -64,7 +67,8 @@ class ContractFileManipulation {
     }
   }
 
-  String _replacePlaceholders(String content, Map<String, String?> replacements) {
+  String _replacePlaceholders(
+      String content, Map<String, String?> replacements) {
     return replacements.entries.fold(content, (acc, entry) {
       final regex = RegExp('\\{${entry.key}\\}');
       return acc.replaceAll(regex, entry.value ?? '');
@@ -82,9 +86,11 @@ class ContractFileManipulation {
     }
   }
 
-  Future<void> modifyXmlContent(String xmlFilePath, Map<String, String> data) async {
+  Future<void> modifyXmlContent(
+      String xmlFilePath, Map<String, String> data) async {
     final file = File(xmlFilePath);
-    final xmlContent = await file.readAsString(encoding: utf8); // Garante UTF-8 ao ler o XML
+    final xmlContent =
+        await file.readAsString(encoding: utf8);
 
     final document = XmlDocument.parse(xmlContent);
 
@@ -96,14 +102,18 @@ class ContractFileManipulation {
       });
     });
 
-    await file.writeAsString(document.toXmlString(pretty: true, indent: '  '), encoding: utf8); // Garante UTF-8 ao escrever o XML
+    await file.writeAsString(document.toXmlString(pretty: true, indent: '  '),
+        encoding: utf8);
   }
 
   Map<String, String?> getData(Rental rental) {
+    double valor = parseValor(rental.paymentValue);
+    String valorPorExtenso = convertDecimalToWords(valor);
     return {
       'nome': rental.naturalPerson?.name,
       'cpf': rental.naturalPerson?.cpf,
-      'endereco': '${rental.naturalPerson?.street}, ${rental.naturalPerson?.addressNumber}, bairro ${rental.naturalPerson?.neighborhood}, ${rental.naturalPerson?.addressComplement}',
+      'endereco':
+          '${rental.naturalPerson?.street}, ${rental.naturalPerson?.addressNumber}, bairro ${rental.naturalPerson?.neighborhood}, ${rental.naturalPerson?.addressComplement}',
       'cidade': rental.naturalPerson?.city,
       'cep': rental.naturalPerson?.cep,
       'estado': rental.naturalPerson?.state,
@@ -113,6 +123,34 @@ class ContractFileManipulation {
       'placa': rental.vehicle?.licensePlate,
       'renavam': rental.vehicle?.renavam.toString(),
       'proprietario': rental.vehicle?.owner,
+      'data': DateFormat('dd/MM/yyyy').format(DateTime.now()),
+      'nomeAssinatura': rental.naturalPerson?.name?.toUpperCase(),
+      'valor': '${rental.paymentValue} ($valorPorExtenso)',
+      'frequencia': getFrequencia(rental.paymentType)
     };
+  }
+
+  String getFrequencia(String? frequencia) {
+    if (frequencia == 'Diário') {
+      return 'dia';
+    } else if (frequencia == 'Semanal') {
+      return 'semana';
+    } else if (frequencia == 'Mensal') {
+      return 'mês';
+    } else {
+      return 'ano';
+    }
+  }
+
+  double parseValor(String? valor) {
+    final regex = RegExp(r'^R\$ ?');
+    String? valorSemR = valor?.replaceAll(regex, '');
+
+    valorSemR = valorSemR?.replaceAll('.', '').replaceAll(',', '.');
+    return double.parse(valorSemR!);
+  }
+
+  String convertDecimalToWords(double numero) {
+    return extenso(numero);
   }
 }
